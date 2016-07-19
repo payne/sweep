80839f480d07407ce19b5b6b5f143a2980e12e8c
Matt Payne <Payne@MattPayne.org>
2016-07-18 21:49:36 -0500

    ./deploy.sh # now builds out folder for game

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,12 @@ CURRENT_COMMIT=`git rev-parse HEAD`
 
 npm install typescript@1.3.0 -g
 
-echo "Cloning gh-pages branch..."
+#echo "Cloning gh-pages branch..."
 
-git clone -b gh-pages "https://${GH_TOKEN}@${GH_REF}" out > /dev/null 2>&1 || exit 1
+#git clone -b gh-pages "https://${GH_TOKEN}@${GH_REF}" out > /dev/null 2>&1 || exit 1
 
 rm -r out/*
+mkdir out
 
 echo "Compiling TSC"
 
@@ -20,16 +21,16 @@ cp -r ./Src/* out
 cd out
 rm Src.csproj
 
-echo "Setting commit number ${CURRENT_COMMIT}"
-sed -i "s/COMMIT_NUMBER/${CURRENT_COMMIT}/g" ./index.html
-
-echo "Committing and pushing to GH"
-
-git config user.name "Travis-CI"
-git config user.email "travis@excaliburjs.com"
-git add -A
-git commit --allow-empty -m "Deploying game for $CURRENT_COMMIT" || exit 1
-git push origin gh-pages > /dev/null 2>&1 || exit 1
-
-echo "Pushed deployment successfully"
+#echo "Setting commit number ${CURRENT_COMMIT}"
+#sed -i "s/COMMIT_NUMBER/${CURRENT_COMMIT}/g" ./index.html
+#
+#echo "Committing and pushing to GH"
+#
+#git config user.name "Travis-CI"
+#git config user.email "travis@excaliburjs.com"
+#git add -A
+#git commit --allow-empty -m "Deploying game for $CURRENT_COMMIT" || exit 1
+#git push origin gh-pages > /dev/null 2>&1 || exit 1
+#
+#echo "Pushed deployment successfully"
 exit 0
